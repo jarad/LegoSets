36780916c691da62f66df2018189a73882590b41
jarad <niemi@iastate.edu>
2016-11-05 10:55:57 -0500

    Use data downloaded from [brickset.com](http://brickset.com/sets/mycollection-owned)

--- R/data.R
+++ R/data.R
@@ -2,6 +2,24 @@
 #'
 #' ids for owned data sets
 #'
-#' @format A data.frame with 1 variable: \code{id}
+#' @format A data.frame with 18 variables:
+#'   \code{SetID}
+#'   \code{Number}
+#'   \code{Variant}
+#'   \code{Theme}
+#'   \code{Subtheme}
+#'   \code{Year}
+#'   \code{Name}
+#'   \code{Minifigs}
+#'   \code{Pieces}
+#'   \code{UKPrice}
+#'   \code{USPrice}
+#'   \code{CAPPrice}
+#'   \code{EUPrice}
+#'   \code{ImageURL}
+#'   \code{Owned}
+#'   \code{Wanted}
+#'   \code{QtyOwned}
+#'   \code{Rating}
 #'
 "owned"
--- data-raw/owned.R
+++ data-raw/owned.R
@@ -1,7 +1,10 @@
 library(dplyr)
 
+# owned.csv is cut-and-pasted from
+# http://brickset.com/sets/mycollection-owned
+
 owned = readr::read_csv("owned.csv") %>%
   unique() %>%
-  arrange(id)
+  arrange(SetID)
 
 devtools::use_data(owned, overwrite = TRUE)
